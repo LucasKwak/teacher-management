cd561b29b1f9f316c1effdbca1fc7fe5b5e82b73
LucasKwak <lkwakiglesia@gmail.com>
2024-07-02 23:46:11 +0200

    [COMP-1]: Modify TeacherForm. Add the logic of the form.

--- src/components/TeacherForm.vue
+++ src/components/TeacherForm.vue
@@ -6,27 +6,94 @@
                 <legend>Información del profesor</legend>
                 <div class="teacher-form__wrapper-input">
                     <label class="teacher-form__label" for="nombre">Nombre</label>
-                    <input class="teacher-form__input" type="text" id="nombre">
+                    <input class="teacher-form__input" type="text" id="nombre" v-model="teacher.name">
                 </div>
                 <div class="teacher-form__wrapper-input">
                     <label class="teacher-form__label" for="apellidos">Apellidos</label>
-                    <input class="teacher-form__input" type="text" id="apellidos">
+                    <input class="teacher-form__input" type="text" id="apellidos" v-model="teacher.surname">
                 </div>
                 <div class="teacher-form__wrapper-input">
                     <label class="teacher-form__label" for="dni">DNI</label>
-                    <input class="teacher-form__input" type="text" id="dni">
+                    <input class="teacher-form__input" type="text" id="dni" v-model="teacher.dni">
                 </div>
                 <div class="teacher-form__wrapper-input teacher-form__materias">
                     <label class="teacher-form__label" for="materias">Materias</label>
-                    <input class="teacher-form__input" type="text" id="materias">
-                    <button class="materias__button">Añadir</button>
+                    <input class="teacher-form__input" type="text" id="materias" v-model="subject">
+                    <button class="form__button" title="Añadir materia" type="button" @click="addSubject">Añadir</button>
+                </div>
+                <div class="teacher-form__wrapper-input teacher-form__materias-añadidas">
+                    <p>Materias añadidas</p>
+                    <ul>
+                        <li v-for="(subj, index) in teacher.subjects" :key="index">{{ subj }}</li>
+                    </ul>
+                </div>
+                <div class="teacher-form__wrapper-input teacher-form__materias">
+                    <label class="teacher-form__label" for="documentacion">Documentación entregada</label>
+                    <input class="teacher-form__input" type="checkbox" id="documentacion"  v-model="teacher.documentation">
                 </div>
             </fieldset>
+            <button class="form__button" type="button" @click="addTeacher">Añadir profesor</button>
         </form>
+    </section>
+
+    <section>
+        <h2>LISTADO DE PROFESORES DADOS DE ALTA</h2>
     </section>
 </template>
 
 <script lang="ts" setup>
+    import { Ref, ref } from 'vue';
+
+    interface ITeacher {
+        name: string,
+        surname: string,
+        dni: string,
+        subjects: Array<string>,
+        documentation: boolean
+    }
+
+    // ARRAY DE PROFESORES GLOBAL
+    let teachers: Ref<Array<ITeacher>> = ref([]);
+    
+    // PROFESOR POR CADA USO DEL FORMULARIO
+    let teacher:Ref<ITeacher> = ref({
+        name: '',
+        surname: '',
+        dni: '',
+        subjects: [],
+        documentation: false
+    });
+    
+    // MATERIA POR CADA USO DEL INPUT DE MATERIAS (se añade a las materias de profesor correspondiente)
+    let subject: Ref<string> = ref('');
+
+    // FUNCION PARA AÑADIR UNA MATERIA A UN PROFESOR
+    function addSubject() {
+        if(subject.value == ''){
+            alert("Escriba una materia primero");
+        }else{
+            teacher.value.subjects.push(subject.value);
+            subject.value = '';
+        }
+    }
+
+    // FUNCION PARA AÑADIR UN PROFESOR
+    function addTeacher() {
+        teachers.value.push(
+            {
+                name: teacher.value.name,
+                surname: teacher.value.surname,
+                dni: teacher.value.dni,
+                subjects: teacher.value.subjects,
+                documentation: teacher.value.documentation,
+            }
+        );
+        teacher.value.name = '';
+        teacher.value.surname = '';
+        teacher.value.dni = '';
+        teacher.value.subjects = [];
+        teacher.value.documentation = false;
+    }
 
 </script>
 
@@ -39,6 +106,10 @@
 
     .teacher-form {
         width: 500px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 15px;
     }
 
     .teacher-form__wrapper-set {
@@ -55,11 +126,24 @@
         gap: 40px;
     }
 
+    .teacher-form__materias-añadidas {
+        max-height: 200px;
+    }
+
+    .teacher-form__materias-añadidas p {
+        font-style: italic;
+    }
+
+    .teacher-form__materias-añadidas ul {
+        width: 150px;
+        overflow-y: scroll;
+    }
+
     .teacher-form__label {
         width: 100px;
     }
 
-    .materias__button {
+    .form__button {
         font-family: Avenir;
         background-color: blueviolet;
         color: white;
@@ -67,7 +151,7 @@
         outline: 0;
     }
 
-    .materias__button:hover{
+    .form__button:hover{
         background-color: white;
         color: blueviolet;
         border: 1px solid blueviolet;
